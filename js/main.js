4a6e74eb1a12d68bd89e477ef21da846fefbcfde
Mario Guerra <manachoyaso@gmail.com>
2022-04-07 15:59:05 -0400

    footer ending (scroll active need fix)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const navMenu = document.getElementById('nav_Menu');
 const navToggle = document.getElementById('nav_Toggle');
 const navClose = document.getElementById('nav_Close');
 const navlink = document.querySelectorAll('.nav__link');
-
+//show section active var
+const sections = document.querySelectorAll('section[id]')
 //show menu
 if(navToggle){
     navToggle.addEventListener('click',() =>{
@@ -22,3 +23,22 @@ function linkAction(){
     navMenu.classList.remove('showMenu')
 };
 navlink.forEach(n => n.addEventListener('click',linkAction))
+//funcion show your section in nav ARREGLAR
+function scrollActive(){
+    const scrollY = window.pageXOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sections = current.getAttribute('id')
+        
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            console.log('hola')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active_link')
+        }else{
+            console.log('chao')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active_link')
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive)
